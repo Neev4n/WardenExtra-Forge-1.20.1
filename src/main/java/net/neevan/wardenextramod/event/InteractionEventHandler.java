33e5d8b26d02239fefb7d4ecd7303d392a44e67b
nkan0025 <nkan0025@student.monash.edu>
2025-05-08 00:25:24 +1000

    Add event handler to stop player right clicks

---
+++ src/main/java/net/neevan/wardenextramod/event/InteractionEventHandler.java
@@ -0,0 +1,21 @@
+package net.neevan.wardenextramod.event;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.neevan.wardenextramod.WardenExtraMod;
+import net.neevan.wardenextramod.item.ModItems;
+
+@Mod.EventBusSubscriber(modid = WardenExtraMod.MODID)
+public class InteractionEventHandler {
+
+    @SubscribeEvent
+    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        ItemStack mainHand = event.getItemStack();
+        // If the player is holding the InteractWandItem
+        event.setCanceled(true); // Cancels entire pipeline
+        event.setUseBlock(net.minecraftforge.eventbus.api.Event.Result.DENY);
+        event.setUseItem(net.minecraftforge.eventbus.api.Event.Result.DENY);
+    }
+}
